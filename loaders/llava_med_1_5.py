8f4496941d04bbfae1718c9f593034907f83028d
Aranya Saha <139392880+thisisAranya@users.noreply.github.com>
2024-11-12 13:12:19 +0600

    Update llava_med_1_5.py

--- loaders/llava_med_1_5.py
+++ loaders/llava_med_1_5.py
@@ -1,16 +1,16 @@
 from typing import Tuple
 
-from transformers import AutoProcessor, LlavaForConditionalGeneration, PreTrainedTokenizer
+from transformers import AutoProcessor, LlavaMistralForCausalLM, PreTrainedTokenizer
 
 from . import register_loader
 from .base import BaseModelLoader
 
 
-@register_loader("llava-1.5")
+@register_loader("llava-med-1.5")
 class LLaVA15ModelLoader(BaseModelLoader):
-    def load(self, load_model: bool = True) -> Tuple[LlavaForConditionalGeneration, PreTrainedTokenizer, AutoProcessor]:
+    def load(self, load_model: bool = True) -> Tuple[LlavaMistralForCausalLM, PreTrainedTokenizer, AutoProcessor]:
         if load_model:
-            model = LlavaForConditionalGeneration.from_pretrained(
+            model = LlavaMistralForCausalLM.from_pretrained(
                 self.model_local_path, 
                 **self.loading_kwargs,
             )
@@ -21,3 +21,17 @@ class LLaVA15ModelLoader(BaseModelLoader):
         processor = AutoProcessor.from_pretrained(self.model_hf_path)
         tokenizer = processor.tokenizer
         return model, tokenizer, processor
+
+
+
+    
+    # if 'llava' in model_name.lower():
+    #     # Load LLaVA model
+    #         if 'mistral' in model_name.lower():
+    #             tokenizer = AutoTokenizer.from_pretrained(model_path)
+    #             model = LlavaMistralForCausalLM.from_pretrained(
+    #                 model_path,
+    #                 low_cpu_mem_usage=False,
+    #                 use_flash_attention_2=False,
+    #                 **kwargs
+    #             )
