cf7a1951e879e15b6c7db13b4813e43a6af001f7
Aranya Saha <139392880+thisisAranya@users.noreply.github.com>
2024-11-12 13:08:53 +0600

    Create llava_med_1_5.py

---
+++ loaders/llava_med_1_5.py
@@ -0,0 +1,23 @@
+from typing import Tuple
+
+from transformers import AutoProcessor, LlavaForConditionalGeneration, PreTrainedTokenizer
+
+from . import register_loader
+from .base import BaseModelLoader
+
+
+@register_loader("llava-1.5")
+class LLaVA15ModelLoader(BaseModelLoader):
+    def load(self, load_model: bool = True) -> Tuple[LlavaForConditionalGeneration, PreTrainedTokenizer, AutoProcessor]:
+        if load_model:
+            model = LlavaForConditionalGeneration.from_pretrained(
+                self.model_local_path, 
+                **self.loading_kwargs,
+            )
+            model.config.hidden_size = model.language_model.config.hidden_size # useful for deepspeed
+        else:
+            model = None
+
+        processor = AutoProcessor.from_pretrained(self.model_hf_path)
+        tokenizer = processor.tokenizer
+        return model, tokenizer, processor
